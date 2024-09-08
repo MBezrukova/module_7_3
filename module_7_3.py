1a3a0744c9105384b5548bf679610c89b418024d
MBezrukova <marinabezrukova389@gmail.com>
2024-09-08 18:58:41 +0300

    Add files via upload

---
+++ module_7_3.py
@@ -0,0 +1,63 @@
+import string
+
+class WordsFinder:
+    def __init__(self, *file_names):
+        self.file_names = file_names
+
+    def get_all_words(self):
+        all_words = {}
+        for file_name in self.file_names:
+            try:
+                with open(file_name, 'r', encoding='utf-8') as file:
+                    text = file.read().lower()
+                    text = text.translate(str.maketrans('', '', string.punctuation))
+                    words = text.split()
+                    all_words[file_name] = words
+            except FileNotFoundError:
+                print(f"Файл {file_name} не найден.")
+                all_words[file_name] = []
+        return all_words
+
+    def find(self, word):
+        word = word.lower()
+        result = {}
+        all_words = self.get_all_words()
+
+        for file_name, words in all_words.items():
+            if word in words:
+                result[file_name] = words.index(word) + 1
+
+        return result
+
+    def count(self, word):
+        word = word.lower()
+        result = {}
+        all_words = self.get_all_words()
+
+        for file_name, words in all_words.items():
+            count = words.count(word)
+            if count > 0:
+                result[file_name] = count
+
+        return result
+
+
+finder0 = WordsFinder('test_file.txt')
+print(finder0.get_all_words())
+print(finder0.find('TEXT'))
+print(finder0.count('teXT'))
+
+finder1 = WordsFinder('Mother Goose - Monday’s Child.txt',)
+print(finder1.get_all_words())
+print(finder1.find('Child'))
+print(finder1.count('Child'))
+
+finder1 = WordsFinder('Rudyard Kipling - If.txt',)
+print(finder1.get_all_words())
+print(finder1.find('if'))
+print(finder1.count('if'))
+
+finder1 = WordsFinder('Walt Whitman - O Captain! My Captain!.txt')
+print(finder1.get_all_words())
+print(finder1.find('captain'))
+print(finder1.count('captain'))
